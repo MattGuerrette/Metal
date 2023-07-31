5e068dd07e3a9fc8f0bfa8523762b13b9fa1f2aa
Matt Guerrette <direct3Dtutorials@gmail.com>
2023-07-31 00:21:51 -0400

    Move common Metal objects into base example class
    This commit moves the common shared Metal objects into the base example
    class to avoid cluttering each sample with the same device and sync
    object creation code. Now, samples all have access to the device,
    command queue, sync objects, depth stencil buffer. The swapchain image
    for the active frame is passed directly to the render call as a
    parameter.

--- source/base/Example.cpp
+++ source/base/Example.cpp
@@ -28,6 +28,37 @@ Example::Example(const char *title, uint32_t width, uint32_t height) {
     CurrentClockTimestamp = SDL_GetPerformanceCounter();
     Width = width;
     Height = height;
+
+
+    Device = NS::TransferPtr(MTL::CreateSystemDefaultDevice());
+
+    auto *layer = static_cast<CA::MetalLayer *>((SDL_Metal_GetLayer(View)));
+    layer->setDevice(Device.get());
+
+    CommandQueue = NS::TransferPtr(Device->newCommandQueue());
+
+    CreateDepthStencil();
+
+    // Load Pipeline Library
+    CFBundleRef main_bundle = CFBundleGetMainBundle();
+    CFURLRef url = CFBundleCopyResourcesDirectoryURL(main_bundle);
+    char cwd[PATH_MAX];
+    CFURLGetFileSystemRepresentation(url, TRUE, (UInt8 *) cwd, PATH_MAX);
+    CFRelease(url);
+    auto path = NS::Bundle::mainBundle()->resourcePath();
+    NS::String *library = path->stringByAppendingString(
+            NS::String::string("/default.metallib", NS::ASCIIStringEncoding));
+
+    NS::Error *error = nullptr;
+    PipelineLibrary = NS::TransferPtr(Device->newLibrary(library, &error));
+    if (error != nullptr) {
+        fprintf(stderr, "Failed to create pipeline library: %s\n",
+                error->localizedFailureReason()->utf8String());
+        abort();
+    }
+
+
+    FrameSemaphore = dispatch_semaphore_create(BUFFER_COUNT);
 }
 
 Example::~Example() {
@@ -35,6 +66,28 @@ Example::~Example() {
         SDL_DestroyWindow(Window);
     }
     SDL_Quit();
+}
+
+void Example::CreateDepthStencil() {
+    MTL::DepthStencilDescriptor *depthStencilDescriptor = MTL::DepthStencilDescriptor::alloc()->init();
+    depthStencilDescriptor->setDepthCompareFunction(MTL::CompareFunctionLess);
+    depthStencilDescriptor->setDepthWriteEnabled(true);
+
+    DepthStencilState = NS::TransferPtr(Device->newDepthStencilState(depthStencilDescriptor));
+
+    depthStencilDescriptor->release();
+
+
+    MTL::TextureDescriptor *textureDescriptor = MTL::TextureDescriptor::texture2DDescriptor(
+            MTL::PixelFormatDepth32Float_Stencil8, GetFrameWidth(), GetFrameHeight(), false);
+    textureDescriptor->setSampleCount(1);
+    textureDescriptor->setUsage(MTL::TextureUsageRenderTarget);
+    textureDescriptor->setResourceOptions(MTL::ResourceOptionCPUCacheModeDefault | MTL::ResourceStorageModePrivate);
+    textureDescriptor->setStorageMode(MTL::StorageModeMemoryless);
+
+    DepthStencilTexture = NS::TransferPtr(Device->newTexture(textureDescriptor));
+
+    textureDescriptor->release();
 }
 
 uint32_t Example::GetFrameWidth() const {
@@ -56,6 +109,8 @@ int Example::Run([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
     }
 
     while (Running) {
+        NS::AutoreleasePool *pool = NS::AutoreleasePool::alloc()->init();
+
         SDL_Event e;
         SDL_PollEvent(&e);
         if (e.type == SDL_QUIT) {
@@ -78,7 +133,23 @@ int Example::Run([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
 
         Update(elapsed);
 
-        Render(elapsed);
+        FrameIndex = (FrameIndex + 1) % BUFFER_COUNT;
+
+        MTL::CommandBuffer *commandBuffer = CommandQueue->commandBuffer();
+
+        dispatch_semaphore_wait(FrameSemaphore, DISPATCH_TIME_FOREVER);
+        commandBuffer->addCompletedHandler([this](MTL::CommandBuffer *buffer) {
+            dispatch_semaphore_signal(FrameSemaphore);
+        });
+
+        auto *layer = static_cast<CA::MetalLayer *>(SDL_Metal_GetLayer(View));
+        CA::MetalDrawable *drawable = layer->nextDrawable();
+        if (drawable) {
+            Render(drawable, elapsed);
+        }
+
+
+        pool->release();
     }
 
     return EXIT_SUCCESS;
--- source/base/Example.hpp
+++ source/base/Example.hpp
@@ -6,6 +6,9 @@
 
 #include <SDL2/SDL.h>
 
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
+
 class Example {
 public:
     Example(const char *title, uint32_t width, uint32_t height);
@@ -22,10 +25,24 @@ public:
 
     virtual void Update(float elapsed) = 0;
 
-    virtual void Render(float elapsed) = 0;
+    virtual void Render(CA::MetalDrawable *drawable, float elapsed) = 0;
 
 protected:
+    static constexpr int BUFFER_COUNT = 3;
+
     SDL_MetalView View;
+
+    // Metal
+    NS::SharedPtr<MTL::Device> Device;
+    NS::SharedPtr<MTL::CommandQueue> CommandQueue;
+    NS::SharedPtr<MTL::Texture> DepthStencilTexture;
+    NS::SharedPtr<MTL::DepthStencilState> DepthStencilState;
+    NS::SharedPtr<MTL::Library> PipelineLibrary;
+
+    // Sync primitives
+    uint32_t FrameIndex = 0;
+    dispatch_semaphore_t FrameSemaphore;
+
 private:
     SDL_Window *Window;
     uint32_t Width;
@@ -33,4 +50,9 @@ private:
     uint64_t LastClockTimestamp;
     uint64_t CurrentClockTimestamp;
     bool Running;
+
+
+    void CreateDepthStencil();;
+
+
 };
