f00dcea88ef274b561be8e532fd5d9be0a1866db
Matt Guerrette <direct3Dtutorials@gmail.com>
2025-03-30 11:15:17 -0400

    Switch to imgui fork

--- cmake/FetchExternal.cmake
+++ cmake/FetchExternal.cmake
@@ -15,3 +15,12 @@ include_directories(${metalcpp_SOURCE_DIR})
 set_target_properties(
         AppleFrameworksCpp
         PROPERTIES FOLDER "External")
+
+FetchContent_Declare(
+    imgui
+    GIT_REPOSITORY https://github.com/MattGuerrette/imgui.git
+    GIT_TAG master
+)
+FetchContent_MakeAvailable(imgui)
+include_directories(${imgui_SOURCE_DIR})
+include_directories(${imgui_SOURCE_DIR}/backends)
